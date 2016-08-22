f3dff49cf4f5e8d21587339411eda04b964eb086
unknown <jitu.ranjan@PDC2LAP-6020HKX.dir.svc.accenture.com>
2016-08-22 14:58:55 +0530

    "changed package name"

--- src/test/java/com/splunktest/clouhub/splunk/test/SplunkDataPushTest.java
+++ src/test/java/com/splunktest/clouhub/splunk/test/SplunkDataPushTest.java
@@ -1,4 +1,4 @@
-package com.accenture.clouhub.splunk.test;
+package com.splunktest.clouhub.splunk.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
